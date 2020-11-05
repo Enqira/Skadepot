69f67034ec095ee2cd0ca0c321afa3a784995a42
Enqira <moenkira@yahoo.com>
2020-11-05 23:16:50 +0100

    initial commit

---
+++ www/js/index.js
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+
+
+
+
+}
+// 05-11 Starts here
+let app = {
+    // in case I dont want to auto lunch camera
+    // init: function() {
+    //     document.getElementById('btn').addEventListener('click', app.takephoto);
+    // },
+    takephoto: function() {
+        let opts = {
+            quality: 20,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            mediaType: Camera.MediaType.PICTURE,
+            encodingType: Camera.EncodingType.JPEG,
+            cameraDirection: Camera.Direction.BACK,
+            targetWidth: 300,
+            targetHeight: 400
+        };
+
+        navigator.camera.getPicture(app.ftw, app.wtf, opts);
+        
+    },
+    ftw: function(imgURI) {
+        document.getElementById('msg').textContent = imgURI;
+        document.getElementById('photo').src = imgURI;
+
+    },
+    wtf: function(msg) {
+        document.getElementById('msg').textContent = msg;
+    }
+};
+
+document.addEventListener('deviceready', app.takephoto);
